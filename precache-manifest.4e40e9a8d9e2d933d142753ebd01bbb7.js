c343d7ccf4a5f76a99963ebe9b98cc853697e631
Filiberto Reyes <filibertoreyes55@gmail.com>
2018-05-22 23:30:46 -0700

    first commit

---
+++ precache-manifest.4e40e9a8d9e2d933d142753ebd01bbb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc48fed85f292ab6b88c",
+    "url": "/js/vendors~app.bc48fed8.js"
+  },
+  {
+    "revision": "ae5fe80d7bcf23cae265",
+    "url": "/js/app.ae5fe80d.js"
+  },
+  {
+    "revision": "5dd7537f2a6dcdea29f383f3d469b73d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae5fe80d7bcf23cae265",
+    "url": "/css/app.925ea013.css"
+  },
+  {
+    "revision": "0a27a4163254fc8fce870c8cc3a3f94f",
+    "url": "/404.html"
+  }
+];
